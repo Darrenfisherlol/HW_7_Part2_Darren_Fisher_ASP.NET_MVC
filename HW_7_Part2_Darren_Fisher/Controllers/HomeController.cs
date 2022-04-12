2f3b221c8e6c5c7139bb6b69fe77b3ba321c9a24
Darrenfisherlol <darren.nov@gmail.com>
2022-04-12 17:31:39 -0500

    Add project files.

---
+++ HW_7_Part2_Darren_Fisher/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HW_7_Part2_Darren_Fisher.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+            return new ContentResult {Content = "This is HW part 7"};
+
+
+        }
+
+        // a/b is the new route instead if SecondPage
+        
+        //[Route("a/b")]
+
+        public IActionResult SecondPage()
+        {
+
+            return View();
+            return new ContentResult { Content = "This is the 2nd webpage in home controller" };
+
+        }
+}
+}
